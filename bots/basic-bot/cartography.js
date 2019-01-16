9a6dd61dda15470ceecd00d15f50304424206dc4
James Rayman <jamsramen@gmail.com>
2019-01-16 17:09:15 -0600

    Update cartography.js

--- bots/basic-bot/cartography.js
+++ bots/basic-bot/cartography.js
@@ -1,12 +1,43 @@
 
 class Cartography {
     constructor(map, karboniteMap, fuelMap, getVisibleRobotMap) {
-        this.map = pos => return map[pos.y][pos.x];
-        this.karboniteMap = pos => return karboniteMap[pos.y][pos.x];
-        this.fuelMap = pos => return fuelMap[pos.y][pos.x];
-        this.robotMap = pos => return getVisibleRobotMap()[pos.y][pos.x];
+        this.size = map.length;
+        this.isPassable = pos => map[pos.y][pos.x];
+        this.isKarboniteMine = pos => karboniteMap[pos.y][pos.x];
+        this.isFuelMine = pos => fuelMap[pos.y][pos.x];
+        this.robotMap = pos => getVisibleRobotMap()[pos.y][pos.x];
+    }
+    isOccupied(pos) {
+        return this.robotMap(pos) > 0;
+    }
+    isOpen(pos) {
+        return !this.isOccupied(pos) && this.map(pos);
+    }
+    isMine(pos) {
+        return this.isKarbonite(pos) || this.isFuelMine(pos);
     }
     
+    isSymmetricX () {
+        for (let x = 0; x < this.size; x++) {
+            for (let y = 0; y * 2 < this.size; y++) {
+                pos = new Point(x, y);
+                if (this.isPassable(pos) != this.isPassible(pos.reflectX(this.size)))
+                    return false;
+            }
+        }
+        return true;
+    }
+
+    isSymmetricY () {
+        for (let x = 0; x < this.size; x++) {
+            for (let y = 0; y * 2 < this.size; y++) {
+                pos = new Point(x, y);
+                if (this.isPassable(pos) != this.isPassible(pos.reflectY(this.size)))
+                    return false;
+            }
+        }
+        return true;
+    }
 }
 
 export default Cartography;
