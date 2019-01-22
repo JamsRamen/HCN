d26c06de0d4984d2e77c922069ab31ee4c4c7c17
James Rayman <jamsramen@gmail.com>
2019-01-22 15:15:43 -0600

    Adds documentation

--- bots/basic-bot/cartography.js
+++ bots/basic-bot/cartography.js
@@ -1,7 +1,44 @@
 /*
  * cartography.js: responsible for low-level map functions, including map access and bounds checking bounds
+ *
+ * class Cartography:
+ * variables:
+ *      none
+ *  
+ * functions:
+ *      Cartography(map, karboniteMap, fuelMap, getVisibleRobotMap)
+ *          arguments are from BCAbstractRobot
+ *
+ *      isPassable(pos)
+ *          returns true if square at pos is passable, false otherwise
+ *      isKarboniteMine(pos)
+ *          returns true if square at pos is a karbonite mine, false otherwise
+ *      isFuelMine(pos)
+ *          returns true if square at pos is a fuel mine, false otherwise
+ *      isOccupied(pos)
+ *          returns true if there is known to be a robot at pos, false otherwise
+ *      isOpen(pos)
+ *          returns true if pos is passable and not occupied, false otherwise
+ *      isMine(pos)
+ *          returns true if square at pos is a mine, false otherwise
+ *      
+ *      robotMap(pos)
+ *          returns -1 if pos is out of site, 0 if pos contains no robot, or the id of the robot at pos
+ *
+ *      isInBounds(pos)
+ *          returns true if pos is in the bounds of the map
+ *
+ *      reflectX(pos)
+ *          return the reflection of pos over the central x-axis of the map
+ *      reflectY(pos)
+ *          return the reflection of pos over the central y-axis of the map
+ *      isSymmetricX()
+ *          returns true if the map is symmetric over the central x-axis
+ *      isSymmetricX()
+ *          returns true if the map is symmetric over the central y-axis
  */
- import Point from './point.js';
+
+import Point from './point.js';
 
 class Cartography {
     constructor(map, karboniteMap, fuelMap, getVisibleRobotMap) {
@@ -48,7 +85,11 @@ class Cartography {
         return this.isKarbonite(pos) || this.isFuelMine(pos);
     }
     
+    // add the following functions here
+    // reflectX(pos)
+    // reflectY(pos)
     
+    // store results of these two functions
     isSymmetricX() {
         for (let x = 0; x < this.size; x++) {
             for (let y = 0; y * 2 < this.size; y++) {
