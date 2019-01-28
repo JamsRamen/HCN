5b101fae0d4d6ab3350031802530f6882e36ae2e
James Rayman <jamsramen@gmail.com>
2019-01-27 19:07:08 -0600

    Fixed all my mistakes

--- bots/basic-bot/defense-role.js
+++ bots/basic-bot/defense-role.js
@@ -1,4 +1,8 @@
 import Role from './role.js';
+import Nav from './nav.js';
+import { SPECS } from 'battlecode';
+
+const findNearest = Nav.findNearest;
 
 class DefenseRole extends Role {
     constructor (context, spawnSignal) {
@@ -14,16 +18,18 @@ class DefenseRole extends Role {
         });
         const castleLoc = this.castleLocations[0];
         
-        this.isDefensePos = (cart, loc) => cart.isOpen(loc) && castleLoc.x - loc.x % 2 == 0 && castleLoc.y - loc.y % 2 == 0;
+        this.isDefensePos = (cart, loc) =>
+            cart.isOpen(loc) && (castleLoc.x - loc.x) % 2 === 0 && (castleLoc.y - loc.y) % 2 === 0;
+        
         this.dest = findNearest(this.me.pos, this.SPEED, this.cartography, this.isDefensePos);
     }
     decide() {
         const attack = this.attackAuto();
         if (attack === undefined) {
-            if (this.me.pos.equals(dest)) {
+            if (this.me.pos.equals(this.dest)) {
                 return undefined;
             }
-            if (!this.cartograph.isOpen(this.dest)) {
+            if (!this.cartography.isOpen(this.dest)) {
                 this.dest = findNearest(this.me.pos, this.SPEED, this.cartography, this.isDefensePos);
             }
             return this.moveTowards(this.dest);
